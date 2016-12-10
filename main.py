a0671e23aef774eed458092df8dc8da6c3237278
Justin Buchanan <justbuchanan@gmail.com>
2016-12-09 21:32:39 -0800

    it works!

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 
 import qrcode
 from reportlab.platypus import Paragraph
-from reportlab.lib.utils import ImageReader
+from reportlab.lib.utils import ImageReader, simpleSplit
+from reportlab.lib.units import inch
+from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfgen import canvas
 import argparse
 import subprocess as proc
+import sys
 
 # setup command-line arguments
 parser = argparse.ArgumentParser("Print labels for dymo label writer, optionally with a qr code")
@@ -20,23 +23,62 @@ qr = qrcode.make(args.qr)
 img = ImageReader(qr._img)
 
 
-# units
-point = 1
-inch = 72 * point
 LABEL_SIZE = (1.125*inch, 3.25*inch)
+# LABEL_SIZE = (0.6*inch, 1.75*inch)
 
 
 # create label
 outfile = "label.pdf"
 c = canvas.Canvas(outfile, pagesize=LABEL_SIZE)
+
+# draw qr code
 c.drawImage(img, 0, 0, LABEL_SIZE[0], LABEL_SIZE[0])
-# canvas.setFont('Times-Roman', 9) 
-c.translate(0, LABEL_SIZE[1])
+
+# font
+fontName = 'Helvetica'
+fontSize = 12
+c.setFont(fontName, fontSize)
+
+
+# font metrics
+def getTextHeight(fontName, fontSize):
+    face = pdfmetrics.getFont(fontName).face
+    ascent = (face.ascent * fontSize) / 1000.0
+    descent = (face.descent * fontSize) / 1000.0
+
+    height = ascent - descent # <-- descent it's negative
+    return height
+
+c.translate(LABEL_SIZE[0], LABEL_SIZE[1])
 c.rotate(-90)
-c.drawString(10, 10, args.text)
+lineSpacing = getTextHeight(fontName, fontSize)
 
+# margins
+leftMargin = 10
+topMargin = leftMargin
 
+c.translate(leftMargin, -topMargin)
+textWidth = LABEL_SIZE[1] - LABEL_SIZE[0] - leftMargin
+
+lines = simpleSplit(args.text, fontName, fontSize, textWidth)
+
+totalHeight = len(lines) * lineSpacing
+
+if totalHeight > LABEL_SIZE[0]:
+    print("ERROR: Text is too big", file=sys.stderr)
+    sys.exit(1)
+
+# center text vertically
+c.translate(0, -(LABEL_SIZE[0] - totalHeight) / 2)
+
+# print each line
+x = 0
+for i in range(len(lines)):
+    c.drawString(x, -lineSpacing*(i), lines[i])
+
+# save label pdf file
 c.save()
+print("Wrote '%s'" % outfile)
 
 # print
 if args.print:
