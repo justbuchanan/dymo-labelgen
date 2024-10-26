c324859cc86abdbe8faef985e87108f61b249cdc
Justin Buchanan <justbuchanan@gmail.com>
2024-10-25 18:55:42 -0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python312.withPackages (
+              ps: with ps; [
+                qrcode
+                reportlab
+              ]
+            ))
+          ];
+        };
+      }
+    );
+}
